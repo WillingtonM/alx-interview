d40fcab76dac9f5423189d82f722055d17770c77
WillingtonM <willington.mhlanga@gmail.com>
2024-01-26 05:22:13 +0200

    0x03. Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,9 +5,9 @@ script that reads stdin line by line & computes metrics:
 import sys
 
 size_total = 0
-counter = 0 
+counter = 0
 status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-                     '404': 0, '405': 0, '500': 0}
+                '404': 0, '405': 0, '500': 0}
 
 
 try:

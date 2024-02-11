7d02064f565de9036e6e429032ef284bcbae0aac
WillingtonM <willington.mhlanga@gmail.com>
2024-02-11 22:30:45 +0200

    0x05. N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,99 +4,63 @@
 """
 import sys
 
-sols = []
-n_size = 0
-postn = None
+
+def backtracks(rw, nm, culumns, pos, neg, board):
+    """
+    function: backtrack to find solution
+    """
+    if rw == nm:
+        res = []
+        for b_len in range(len(board)):
+            for k_len in range(len(board[b_len])):
+                if board[b_len][k_len] == 1:
+                    res.append([b_len, k_len])
+        print(res)
+        return
+
+    for k in range(nm):
+        if k in culumns or (rw + k) in pos or (rw - k) in neg:
+            continue
+
+        culumns.add(k)
+        pos.add(rw + k)
+        neg.add(rw - k)
+        board[rw][k] = 1
+
+        backtracks(rw+1, nm, culumns, pos, neg, board)
+
+        culumns.remove(k)
+        pos.remove(rw + k)
+        neg.remove(rw - k)
+        board[rw][k] = 0
 
 
-def get_input():
+def n_queens(n):
     """
-        Retrieves validates program's argument
-        Returns (int): chessboard size
+    Solution to n_queens problem
+    Args (n): number of queens and must be >= 4
+    Returns: List of lists repr. coordinates of each
+        queen for all possible solutions
     """
-    global n_size
-    n_size = 0
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+    negv_diag = set()
+    posv_diag = set()
+    culumns = set()
+    board = [[0] * n for k in range(n)]
+
+    backtracks(0, n, culumns, posv_diag, negv_diag, board)
+
+
+if __name__ == "__main__":
+    n = sys.argv
+    if len(n) != 2:
+        print("Usage: n_queens N")
         sys.exit(1)
     try:
-        n_size = int(sys.argv[1])
-    except Exception:
+        n_n = int(n[1])
+        if n_n < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+        n_queens(n_n)
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
-    if n_size < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    return n_size
-
-
-def is_attacking(pos_0, pos_1):
-    """
-        Checks if positions of two queens are in an attacking mode.
-        Args:
-            pos_0 (list or tuple): 1st queen's position.
-            pos_1 (list or tuple): 2nd queen's position.
-        Returns (bool): True if queens are in attacking position else False.
-    """
-    if (pos_0[0] == pos_1[0]) or (pos_0[1] == pos_1[1]):
-        return True
-    return abs(pos_0[0] - pos_1[0]) == abs(pos_0[1] - pos_1[1])
-
-
-def group_exists(group):
-    """
-        Checks if group exists in list of solutions.
-        Args: group (list of integers): group of possible positions.
-        Returns (bool): True if exists, False otherwise.
-    """
-    global sols
-    for stn in sols:
-        s_cnt = 0
-        for stn_pos in stn:
-            for grp_pos in group:
-                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
-                    s_cnt += 1
-        if s_cnt == n_size:
-            return True
-    return False
-
-
-def build_sols(row, group):
-    """
-        Builds solution for n queens problem.
-        Args:
-            row (int): Current row in chessboard.
-            group (list): Group of valid positions.
-    """
-    global sols
-    global n_size
-    if row == n_size:
-        tmp_grp = group.copy()
-        if not group_exists(tmp_grp):
-            sols.append(tmp_grp)
-    else:
-        for col in range(n_size):
-            a = (row * n_size) + col
-            sol_matches = zip(list([postn[a]]) * len(group), group)
-            pos_used = map(lambda x: is_attacking(x[0], x[1]), sol_matches)
-            group.append(postn[a].copy())
-            if not any(pos_used):
-                build_sols(row + 1, group)
-            group.pop(len(group) - 1)
-
-
-def get_sols():
-    """
-        Gets solutions for given chessboard size.
-    """
-    global postn, n_size
-    postn = list(map(lambda x: [x // n_size, x % n_size], range(n_size ** 2)))
-    a = 0
-    group_sol = []
-    build_sols(a, group_sol)
-
-
-n_size = get_input()
-get_sols()
-for sol in sols:
-    print(sol)

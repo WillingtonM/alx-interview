cb812c2c470f500dbc3cac887baee1de439a02a0
WillingtonM <willington.mhlanga@gmail.com>
2024-02-19 00:21:32 +0200

    0x06. Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,18 @@
 #!/usr/bin/nodenumFilm
 // A script that prints all characters of Star Wars movie
+
 const request = require('request');
 
-const numFilm = process.argv[2] + '/';
-const endpoint = 'https://swapi-api.hbtn.io/api/films/';
-
-// Makes API request to get film information
-request(endpoint + numFilm, async function (err, res, body) {
-  if (err) return console.error(err);
-
-  // Parse response body to get the list of character URLs
-  const charURLList = JSON.parse(body).characters;
-
-  for (const charURL of charURLList) {
-    await new Promise(function (resolve, reject) {
-      request(charURL, function (err, res, body) {
-        if (err) return console.error(err);
-
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
-  }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const chars = JSON.parse(body).characters;
+  charsOrder(chars, 0);
 });
+const charsOrder = (chars, y) => {
+  if (y === chars.length) return;
+  request(chars[y], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    charsOrder(chars, y + 1);
+  });
+};

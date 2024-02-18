d0e3e4ae9deb7713ac6e0fd1c156c186527eb969
WillingtonM <willington.mhlanga@gmail.com>
2024-02-19 00:45:10 +0200

    0x06. Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,26 @@
-#!/usr/bin/nodenumFilm
-// A script that prints all characters of Star Wars movie
-const req = require('request');
+#!/usr/bin/node
 
-if (process.argv.length !== 3) {
-  console.log('Usage: ./0-starwars_characters.js <n>');
-  process.exit(1);
-}
+const request = require('request');
 
-const proc_id = process.argv[2];
-if (isNaN(proc_id)) {
-  console.log('<n> must be an integer');
-  process.exit(1);
-}
+const numFilm = process.argv[2] + '/';
+const filmURLDATA = 'https://swapi-api.hbtn.io/api/films/';
 
-function promiseRequest (url) {
-  return new Promise((resolve, reject) => {
-    req.get(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      }
-      resolve(JSON.parse(body));
+// Makes API request to get film data
+request(filmURLDATA + numFilm, async function (err, res, body) {
+  if (err) return console.error(err);
+
+  // Parse response body to get list of character URLs
+  const charactorURLList = JSON.parse(body).characters;
+
+  // Iterare through character URLs and factor character information
+  for (const charactorURL of charactorURLList) {
+    await new Promise(function (resolve, reject) {
+      request(charactorURL, function (err, res, body) {
+        if (err) return console.error(err);
+
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-  });
-}
-
-async function dispChars () {
-  const endpoint = `https://swapi-api.alx-tools.com/api/films/${proc_id}`;
-  try {
-    const resp = await promiseRequest(endpoint);
-    const char = resp.characters;
-    for (const character of char) {
-      const charBody = await promiseRequest(character);
-      const name = charBody.name;
-      console.log(name);
-    }
-  } catch (err) {
-    console.error(err);
   }
-}
-
-dispChars();
+});
